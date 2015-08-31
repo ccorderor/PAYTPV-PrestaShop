58436b6aefe63dcf67279f5e316046c87fc2e476
Mikel Martin <mikel@zhenit.com>
2015-08-31 18:00:32 +0200

    [FIX] Author

--- paytpv.php
+++ paytpv.php
@@ -18,6 +18,7 @@
 * versions in the future. If you wish to customize PrestaShop for your
 * needs please refer to http://www.prestashop.com for more information.
 *
+*  @author     Mikel Martin <mmartin@paytpv.com>
 *  @author     Jose Ramon Garcia <jrgarcia@paytpv.com>
 *  @copyright  2015 PAYTPV ON LINE S.L.
 *  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
